58a682f93c9c41bfd566f0dc92fa8baa19f80bbe
Mike Landolfi <mikelandolfi@gmail.com>
2016-04-28 22:11:29 -0700

    Added console logging to tell me where things went horribly wrong.

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ http.listen(3000, function() {
   console.log('listening on *:3000');
 });*/
 app.listen(process.env.PORT || 3000, function() { 
-  console.log("Listening on port %d in %s mode", this.address().port, app.settings.end); 
+  console.log("Listening on port %d in %s mode", this.address().port, app.settings.env); 
 }); 
